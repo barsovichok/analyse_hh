7b503d83ab939329bc06d8d2ec2e09e63d9ba951
tatyanakulagina <tatyana@zipsale.co.uk>
2021-05-18 01:18:29 +0300

    delete test

--- test_get_vacancies.py
+++ test_get_vacancies.py
@@ -8,5 +8,3 @@ def test_response_vacancies(response_vacancies):
     assert 'items' in response_vacancies
 
 
-def test_response_vacancies_id(response_vacancies):
-    assert 'id' in response_vacancies.get('items')
